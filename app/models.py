5bde882c2385543ada517c8eee216d0c88bee8f1
Kevin Mugele <kevin.mugele@gmail.com>
2021-12-03 18:11:04 -0500

    Create new game model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(64))
+    score = db.Column(db.Integer, index=True)
+
+    # def __repr__(self):
+    #     return '<This Game for {}>'.format(self.player_name)
