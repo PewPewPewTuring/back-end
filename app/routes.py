b7298768460ade398f9442bf622a4ab7b80d161c
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-12-04 12:58:43 -0800

    Add testing for games route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import request
+from app.models import Game
 import json
-
+import jsonify
 
 def configure_routes(app):
     @app.route('/')
@@ -10,14 +11,41 @@ def configure_routes(app):
 
     @app.route('/games', methods=['POST'])
     def receive_post():
-        headers = request.headers
         data_string = request.get_data()
         data = json.loads(data_string)
 
-        request_id = data.get('request_id')
         payload = data.get('payload')
 
-        if request_id and payload:
-            return 'ok', 200
+        if payload:
+            def score_calculations():
+                score = 100000
+                if int(payload['time_lapsed']) < 60:
+                    score += 50000
+                elif int(payload['time_lapsed']) < 120:
+                    score += 25000
+                elif int(payload['time_lapsed']) < 180:
+                    score += 10000
+                if int(payload['moves_taken']) < 50:
+                    score += 50000
+                elif int(payload['moves_taken']) < 75:
+                    score += 25000
+                elif int(payload['moves_taken']) < 100:
+                    score += 10000
+                score += int(payload['hidden_items_found'])*10000
+                return score
+
+            game = Game(player_name = payload['player_name'], score = score_calculations())
+
+            game_json = {
+            "player_name": game.player_name,
+            "score": game.score
+            }
+
+            response = app.response_class(
+                response=json.dumps(game_json),
+                status=200,
+                mimetype="application/json"
+            )
+            return response
         else:
             return 'bad request', 400
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,3 +11,28 @@ def test_base_route():
     response = client.get(url)
     assert response.get_data() == b"Hello, World!"
     assert response.status_code == 200
+
+def test_post_route_success():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/games'
+
+    mock_request_data = {
+        "payload": {
+            "player_name": "AAA",
+            "time_lapsed": "110",
+            "moves_taken": "50",
+            "hidden_items_found": "2"
+        }
+    }
+
+    mock_game_response = {
+        "player_name": "AAA",
+        "score": 170000}
+
+    response = client.post(url, data=json.dumps(mock_request_data))
+    assert response.status_code == 200
+    response_data_string = response.get_data()
+    response_data = json.loads(response_data_string)
+    assert response_data == mock_game_response
