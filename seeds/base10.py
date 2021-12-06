5fed92036784d1bacbf221cc28dede3919ec4099
danembb <dbrophy720@gmail.com>
2021-12-06 16:45:14 -0700

    Update requirements and push imports to application file

---
+++ seeds/base10.py
@@ -0,0 +1,27 @@
+from flask_seeder import Seeder
+from app.models import Game
+
+class DemoSeeder(Seeder):
+    def run(self):
+        print("seeding ten games...")
+        game01 = Game(player_name="AAA", score=0)
+        game02 = Game(player_name="AAA", score=10000)
+        game03 = Game(player_name="AAA", score=20000)
+        game04 = Game(player_name="AAA", score=30000)
+        game05 = Game(player_name="AAA", score=70000)
+        game06 = Game(player_name="AAA", score=50000)
+        game07 = Game(player_name="AAA", score=40000)
+        game08 = Game(player_name="AAA", score=80000)
+        game09 = Game(player_name="AAA", score=90000)
+        game10 = Game(player_name="AAA", score=60000)
+        self.db.session.add(game01)
+        self.db.session.add(game02)
+        self.db.session.add(game03)
+        self.db.session.add(game04)
+        self.db.session.add(game05)
+        self.db.session.add(game06)
+        self.db.session.add(game07)
+        self.db.session.add(game08)
+        self.db.session.add(game09)
+        self.db.session.add(game10)
+        print("seeding completed!")
