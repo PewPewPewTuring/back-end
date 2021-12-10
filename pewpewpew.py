14da6847ce2eec0406e176fb05aa2aef48034a1e
Kevin Mugele <kevin.mugele@gmail.com>
2021-12-10 17:34:00 -0500

    Update pewpewpew file to remove if statement from leaderboard

--- pewpewpew.py
+++ pewpewpew.py
@@ -38,78 +38,68 @@ def configure_routes(app):
     def get_leaderboard():
         games_array = Game.query.order_by(Game.score.desc()).limit(10).all()
 
-        if len(games_array) < 10:
-            error = 'You do not have a full leaderboard to display'
-            response = app.response_class(
-                response=json.dumps(error),
-                status=200,
-                mimetype="application/json"
-            )
+        leaderboard_json = {
+            "games": [
+                    {
+                    "id": games_array[0].id,
+                    "player_name": games_array[0].player_name,
+                    "score": games_array[0].score
+                },
+                {
+                    "id": games_array[1].id,
+                    "player_name": games_array[1].player_name,
+                    "score": games_array[1].score
+                },
+                {
+                    "id": games_array[2].id,
+                    "player_name": games_array[2].player_name,
+                    "score": games_array[2].score
+                },
+                {
+                    "id": games_array[3].id,
+                    "player_name": games_array[3].player_name,
+                    "score": games_array[3].score
+                },
+                {
+                    "id": games_array[4].id,
+                    "player_name": games_array[4].player_name,
+                    "score": games_array[4].score
+                },
+                {
+                    "id": games_array[5].id,
+                    "player_name": games_array[5].player_name,
+                    "score": games_array[5].score
+                },
+                {
+                    "id": games_array[6].id,
+                    "player_name": games_array[6].player_name,
+                    "score": games_array[6].score
+                },
+                {
+                    "id": games_array[7].id,
+                    "player_name": games_array[7].player_name,
+                    "score": games_array[7].score
+                },
+                {
+                    "id": games_array[8].id,
+                    "player_name": games_array[8].player_name,
+                    "score": games_array[8].score
+                },
+                {
+                    "id": games_array[9].id,
+                    "player_name": games_array[9].player_name,
+                    "score": games_array[9].score
+                }
+            ]
+        }
 
-            return response
-        else:
-            leaderboard_json = {
-                "games": [
-                        {
-                        "id": games_array[0].id,
-                        "player_name": games_array[0].player_name,
-                        "score": games_array[0].score
-                    },
-                    {
-                        "id": games_array[1].id,
-                        "player_name": games_array[1].player_name,
-                        "score": games_array[1].score
-                    },
-                    {
-                        "id": games_array[2].id,
-                        "player_name": games_array[2].player_name,
-                        "score": games_array[2].score
-                    },
-                    {
-                        "id": games_array[3].id,
-                        "player_name": games_array[3].player_name,
-                        "score": games_array[3].score
-                    },
-                    {
-                        "id": games_array[4].id,
-                        "player_name": games_array[4].player_name,
-                        "score": games_array[4].score
-                    },
-                    {
-                        "id": games_array[5].id,
-                        "player_name": games_array[5].player_name,
-                        "score": games_array[5].score
-                    },
-                    {
-                        "id": games_array[6].id,
-                        "player_name": games_array[6].player_name,
-                        "score": games_array[6].score
-                    },
-                    {
-                        "id": games_array[7].id,
-                        "player_name": games_array[7].player_name,
-                        "score": games_array[7].score
-                    },
-                    {
-                        "id": games_array[8].id,
-                        "player_name": games_array[8].player_name,
-                        "score": games_array[8].score
-                    },
-                    {
-                        "id": games_array[9].id,
-                        "player_name": games_array[9].player_name,
-                        "score": games_array[9].score
-                    }
-                ]
-            }
+        response = app.response_class(
+            response=json.dumps(leaderboard_json),
+            status=200,
+            mimetype="application/json"
+        )
 
-            response = app.response_class(
-                response=json.dumps(leaderboard_json),
-                status=200,
-                mimetype="application/json"
-            )
-
-            return response
+        return response
 
     @app.route('/api/v1/games', methods=['POST'])
     def receive_post():
