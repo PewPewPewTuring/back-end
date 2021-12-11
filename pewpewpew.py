6be860ec568538cc98cd07f667e7699416450957
danembb <dbrophy720@gmail.com>
2021-12-10 17:43:53 -0700

    Update pewpewpew to save post requests

--- pewpewpew.py
+++ pewpewpew.py
@@ -128,8 +128,8 @@ def configure_routes(app):
 
             game = Game(player_name = payload['player_name'], score = score_calculations())
             
-            # db.session.add(game)
-            # db.session.commit()
+            db.session.add(game)
+            db.session.commit()
 
             game_json = {
             "player_name": game.player_name,
